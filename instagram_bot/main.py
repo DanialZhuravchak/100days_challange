ad229d6c35b1eb9ff369e79ae5386b31e8191b33
DanialZhuravchak <zhuravchak.danial@gmail.com>
2020-05-02 11:14:44 -0700

    Add old projects

---
+++ instagram_bot/main.py
@@ -0,0 +1,42 @@
+from time import sleep
+from selenium import webdriver 
+from secrets import username,pw
+
+class InstaBot:
+    def __init__(self, username, pw):
+        self.driver = webdriver.Chrome()
+        self.username = username
+
+
+        self.driver.get("https:instagram.com")
+        sleep(2)
+        self.driver.find_element_by_xpath("//a[contains(text(), 'Log in')]")\
+            .click()
+        sleep(2)
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
+            .send_keys(username)
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]")\
+            .send_keys(pw)
+        self.driver.find_element_by_xpath('//button[@type="submit"]')\
+            .click()
+        sleep(4)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+            .click()
+        sleep(2)
+
+    def get_unfollowers(self):
+        self.driver.find_element_by_xpath("//a[contains(@href,'/{}')]".format(self.username))\
+            .click()
+        sleep(2)
+        self.driver.find_element_by_xpath("//a[contains(@href,'/following')]")\
+            .click()
+        following = self._get_names()
+        self.driver.find_element_by_xpath("//a[contains(@href,'/followers')]")\
+            .click()
+        followers = self._get_names()
+        not_following_back = [user for user in following if user not in followers]
+        print(not_following_back)
+
+
+my_bot = InstaBot(username,pw)
+my_bot.get_unfollowers()
---
+++ photo_extract/main.py
@@ -0,0 +1,4 @@
+"""Main Python module for programm that extracts metadata from photos."""
+import sys
+a = 2+2
+b = 3+3
